62921e79599764755c2618f47d82f2ee26ffa62e
mentiferous <226847710+mentiferous@users.noreply.github.com>
2025-09-12 06:28:59 -0400

    update cli.py and pwd.py

--- src/pwdgen/cli.py
+++ src/pwdgen/cli.py
@@ -1,3 +1,23 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# pwdgen - A secure password generator
+# Copyright (C) 2025 mentiferous
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>
+
+"""cli.py - Main module for pwdgen."""
+
 import argparse
 
 from pwdgen import __version__
@@ -5,7 +25,10 @@ from pwdgen.gen.pwd import gen_base85, gen_digits, gen_hex
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="pwdgen")
+    parser = argparse.ArgumentParser(
+        prog="pwdgen",
+        description="A secure password generator",
+    )
 
     parser.add_argument(
         "-V",
