fef4e3ee43261fcee26bf07b4458821df2879f99
mentiferous <226847710+mentiferous@users.noreply.github.com>
2025-10-02 19:01:32 -0400

    update helper.py

--- src/pwdgen/helper.py
+++ src/pwdgen/helper.py
@@ -1,8 +1,30 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# pwdgen - A secure password generator
+# Copyright (C) 2025 mentiferous
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>
+
+"""helper.py - Helper functions for pwdgen."""
+
 from rich.console import Console
 from rich.table import Table
 
 
 def pwd_table(name, password, length):
+    """Print a password table."""
+
     console = Console()
 
     table = Table()
